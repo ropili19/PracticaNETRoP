718645a5bfd54148643c133fb49c38631578cdc6
ropili19 <rosariopili26@gmail.com>
2019-04-28 19:54:30 +0200

    commit initial in this report

---
+++ PracticaNETRoP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaNETRoP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
